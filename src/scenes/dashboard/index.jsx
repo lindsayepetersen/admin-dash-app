bcb20f8969933369cc507df8e773800d8373f185
[lindsayepetersen] <lindsayepetersen@gmail.com>
2023-08-30 17:52:02 -0600

    added invoices

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -2,11 +2,13 @@ import { Box } from "@mui/material";
 import Header from "../../Components/Header";
 
 const Dashboard = () => {
-  return <Box m="20px">
+  return ( 
+  <Box m="20px">
     <Box display="flex" justifyContent="space-between" alignItems="center">
       <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
     </Box>
-  </Box>;
+  </Box>
+  );
 };
 
 export default Dashboard;
